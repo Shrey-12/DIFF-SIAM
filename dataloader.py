6b883a89f7553b7639074470e17316119cd7b82d
Shrey-12 <shreya120malik@gmail.com>
2024-03-07 17:24:02 +0530

    trying to find sizee

--- dataloader.py
+++ dataloader.py
@@ -88,11 +88,7 @@ class DatasetFolder(VisionDataset):
             if self.features and not self.load_images:
                 # load only feature maps
                 # load feature tensors
-                print(load(path).unsqueeze(0).detach().shape)
-                print(load(path.replace("/features_scale_1/", "/features_scale_2/")).detach().shape)
-                      
                 sample = cat((load(path).unsqueeze(0).detach(), load(path.replace("/features_scale_1/", "/features_scale_2/")).detach()), dim=0)
-                print("path:",sample.shape)
 
             elif self.load_images and not self.features: # load only images
                 # print("here2")
